e19c219ec6d80f85564f603366ed3e2ea0de3cde
randymcmillan <randymcmillan@protonmail.com>
2026-03-17 12:45:01 -0400

    n

--- ocean_stats.sh
+++ ocean_stats.sh
@@ -6,9 +6,13 @@ pip install requests
 pip install tqdm
 pip install aiohttp
 pip install argparse
+
+./ocean_total.py
+
+
 #./ocean_stats.py
 #./ocean_health.py
 #./ocean_reward_delta.py
-./ocean_total.py
+#./ocean_total.py
 #./fetch_pools.py
 #./ocean_total_loss.py
